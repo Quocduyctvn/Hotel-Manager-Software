b12f0bb38f759ac32cbee4a781fe4d2b5b0d9a45
Quocduyctvn <quocduyctvn@gmail.com>
2024-12-27 23:14:24 +0700

    View share

---
+++ Hotel.Client/Views/Shared/Components/Introduce/IntroduceViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Client.Views.Shared.Components.Introduce
+{
+    public class IntroduceViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hotel.Client/Views/Shared/Components/MapVietNam/MapVietNamViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Client.Views.Shared.Components.MapVietNam
+{
+    public class MapVietNamViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
