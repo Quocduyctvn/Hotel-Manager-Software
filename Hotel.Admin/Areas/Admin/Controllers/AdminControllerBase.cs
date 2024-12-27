ad0ec18d4fd94c07b0fa1b8961db491f415a2f66
Quocduyctvn <quocduyctvn@gmail.com>
2024-12-27 23:07:27 +0700

    layout

---
+++ Hotel.Admin/Areas/Admin/Controllers/AdminControllerBase.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Hotel.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Admin.Areas.Admin.Controllers
+{
+	[Authorize]
+	public class AdminControllerBase : Controller
+	{
+		protected readonly ApplicationDbContext _HotelDbContext;  // khai báo protected quy định không cho phép truy cập ngoài lớp - trừ kế thừa 
+		protected const int DEFAULT_PAGE_SIZE = 10; // Số phần tử trên 1 trang 
+		protected const int DEFAULT_PAGE_NUMBER = 1;
+		protected const int DEFAULT_PAGE_NUMBER_QUERY_STRING = 1;
+
+		protected readonly IMapper _mapper;
+		public AdminControllerBase(ApplicationDbContext DbContext, IMapper mapper)
+		{
+			_HotelDbContext = DbContext;
+			_mapper = mapper;
+		}
+
+
+		protected void SetErrorMesg(string mesg, bool modelStateIsInvalid = false)
+		{
+			TempData["Err"] = mesg;
+		}
+		protected void SetWrnMesg(string mesg, bool modelStateIsInvalid = false)
+		{
+			TempData["Wrn"] = mesg;
+		}
+
+		protected void SetSuccessMesg(string mesg) => TempData["Success"] = mesg;
+	}
+}
+
---
+++ Hotel.Admin/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Hotel.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Admin.Areas.Admin.Controllers
+{
+    public class AdminHomeController : AdminControllerBase
+    {
+        public AdminHomeController(ApplicationDbContext DbContext, IMapper mapper) : base(DbContext, mapper)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
