35383ceb7e3577ec78b4917f403df32441a9579f
Quocduyctvn <quocduyctvn@gmail.com>
2024-12-27 23:04:31 +0700

    handle_account

---
+++ Hotel.Admin/Areas/Admin/Controllers/AdminAccountController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Hotel.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Admin.Areas.Admin.Controllers
+{
+	public class AdminAccountController : AdminControllerBase
+	{
+		public AdminAccountController(ApplicationDbContext DbContext, IMapper mapper) : base(DbContext, mapper)
+		{
+		}
+
+		public async Task<IActionResult> Logout()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("", "", new { area = "" });
+		}
+	}
+}
